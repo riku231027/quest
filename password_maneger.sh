3b0fc31cfc3b9e84ca9e1e09574f4d9a17839b01
Riku231017 <Riku20231027@gmail.com>
2023-10-31 17:58:47 +0900

    password.txtを追加しました

--- password_maneger.sh
+++ password_maneger.sh
@@ -9,7 +9,7 @@ read username
 echo -n "パスワードを入力してください: "
 read password
 
-echo "$service:$username:$password" >> password_files.md
+echo "$service:$username:$password" >> password.txt
 
 echo -e "Thank you\e[31m!\e[m"
 
